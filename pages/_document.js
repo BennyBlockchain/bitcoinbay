7660ef909834036ed67506b5786958a2c5cdae04
Benny Blockchain <ben@styng.social>
2022-02-28 22:28:40 -0500

    Welcome to the Bay

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link type="text/javascript" src="../clock/flipdown.js" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
