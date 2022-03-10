378f0350ed5df85dec1f836ef2f4a02c17d37269
Benny Blockchain <ben@styng.social>
2022-03-09 21:50:58 -0500

    Animated background

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,18 @@ export default function Document() {
     <Html>
       <Head>
         <link type="text/javascript" src="../clock/flipdown.js" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossorigin="anonymous"
+        ></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
